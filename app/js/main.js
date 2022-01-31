9840c6f8883e930a87a86565e3ff4e2242352ded
vladian95 <kvankvan1995@gmail.com>
2022-01-31 22:45:13 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,8 @@
-$(function(){
-
-
-
-    
-});
+const burgerMenu = document.querySelector('.header__burger-menu');
+if(burgerMenu) {
+    const menuList = document.querySelector('.menu');
+    burgerMenu.addEventListener('click', function(){
+        burgerMenu.classList.toggle('menu--active')
+        menuList.classList.toggle('menu--active')
+    })
+}
